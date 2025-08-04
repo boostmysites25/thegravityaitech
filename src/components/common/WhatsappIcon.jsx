4b75cf1aa7d9a0f01a11c30e0cc1b361c5db2530
Pranav <mpranavprem@gmail.com>
2025-08-04 12:02:30 +0530

    changes

--- src/components/common/WhatsappIcon.jsx
+++ src/components/common/WhatsappIcon.jsx
@@ -4,7 +4,7 @@ import { companyDetails } from '../../content/constant'
 
 const WhatsappIcon = () => {
     return (
-        <Link className='fixed bottom-16 right-4 z-20 hover:scale-105 hover:-translate-y-1 transition-all duration-300' to={`https://wa.me/${companyDetails.phone.replace(/[+\s-]/g, "")}?text=Hi%2C+I'm+interested+in+a+job+%7B${companyDetails.companyName}%7D+(Company))}`} target='_blank'>
+        <Link className='fixed bottom-16 right-4 z-20 hover:scale-105 hover:-translate-y-1 transition-all duration-300' to={`https://wa.me/${companyDetails.phone.replace(/[+\s-]/g, "")}`} target='_blank'>
             <div className="rounded-full p-3 bg-green-500">
                 <IoLogoWhatsapp size={50} className="text-white" />
             </div>
